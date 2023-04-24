e060533f34317cea950df4feaf27b74efe63379d
Fayenator <alexa.schmutz@gmail.com>
2023-04-24 14:14:08 +0200

    change how ingame objects are handled

--- js/main.js
+++ js/main.js
@@ -1,120 +1,138 @@
-import {examineObject, interactObject, takeObject, useWithObject, } from "./objectStringsRoomOne.js"
+// CONSTANTS 
 
-// KEEP FOCUS ON INPUT 
-
-document.getElementById("userInput").onblur = function() {
-    userInput.focus();
-};
-
-
-// INITIATE FIRST DESCRIPTOR PARAGRAPH
-
-document.getElementById("descriptorParagraph").innerHTML = 
-examineObject.initInitRoom;
-
-
-//  CONSTANTS 
+import { firstDoor, goldenKey, smallTable, objectCombinations, } from "./objectStringsRoomOne.js";
 
 const userInput = document.getElementById("userInput");
 
-const objectExamine = ["look", "Look", "examine", "Examine", "inspect","Inspect",];
+const objectExamine = ["look", "Look", "examine", "Examine", "inspect","Inspect", ];
 
 const objectInteract = ["open", "interact", "use", "try", ]
 
 const objectTake = ["take", ]
 
-const objectUseWith = ["use", "combine"]
+const objectUseWith = ["use", "combine", ]
 
 
-//  PLAYER INVENTORY
+// PLAYER INVENTORY
 
-var playerInventory = {
+let playerInventory = [
+
+]
+
+// MAPS
+
+const roomOne = new Map([
+    
+    ["table", smallTable],
+    ["key", goldenKey],
+    ["door", firstDoor],
+
+])
+
+let currentRoom = roomOne
+
+
+let parseUserInput = () =>{
+    const userCommand = document.getElementById("userInput").value;
+    let numberOfWords = userCommand.split(" ").length
+    console.log(numberOfWords)
+    let userCommandVerb = userCommand.split(" ").shift();
+    let userCommandObject = userCommand.split(" ")[1];
+    let userCommandObjectTwo = userCommand.split(" ").pop();
+
+    return {verb: userCommandVerb, object: userCommandObject, objectTwo: userCommandObjectTwo, number: numberOfWords}}
+
+
+// DETERMINES ACTION
+
+let interpretUserInput = (userInput) => {
+
+   
+
+    if (userInput.number > 2) {
+        const objectCombination = objectCombinations.get(JSON.stringify([currentRoom.get(userInput.object), currentRoom.get(userInput.objectTwo)]))
+        // console.log(objectCombinations)
+
+        // USE OBJECT ON OBJECT
+        if (objectUseWith.includes(userInput.verb) && objectCombination != null && playerInventory.includes(userInput.object)) {
+
+            document.getElementById("textOutput").innerHTML = 
+            objectCombination;
+        }
+        else if (objectUseWith.includes(userInput.verb) && objectCombination != null) {
+
+            document.getElementById("textOutput").innerHTML = `You don't have a ${userInput.object}.`
+
+        }
+
+        else {
+
+            document.getElementById("textOutput").innerHTML = "I don't know what you are trying to do.";
+        }
+    }
+        
+    else { 
+        // EXAMINE TABLE
+        if (objectExamine.includes(userInput.verb) && currentRoom.has(userInput.object)) {
+
+            document.getElementById("textOutput").innerHTML = 
+            currentRoom.get(userInput.object).examineObject;
+        }
+
+        // INTERACT
+        else if (objectInteract.includes(userInput.verb) && currentRoom.has(userInput.object)) {
+            if (currentRoom.get(userInput.object).hasOwnProperty("interactObject")) {
+                document.getElementById("textOutput").innerHTML = 
+                currentRoom.get(userInput.object).interactObject;
+            }
+
+            else {
+                document.getElementById("textOutput").innerHTML = `You can't use the ${userInput.object}.`
+            }
+        }
+
+        // TAKE
+        else if (objectTake.includes(userInput.verb)) { 
+
+            if (currentRoom.has(userInput.object)) {
+
+                if (currentRoom.get(userInput.object).hasOwnProperty("takeObject")) {
+                    document.getElementById("textOutput").innerHTML = 
+                    currentRoom.get(userInput.object).takeObject;
+                    playerInventory += userInput.object
+                    console.log(JSON.stringify(playerInventory));
+                }
+                //  CAN'T TAKE
+                else {
+                    document.getElementById("textOutput").innerHTML = `You can't take the ${userInput.object}.`;
+                }
+            }
+            // OBJECT DOESNT EXIST
+            else {
+                document.getElementById("textOutput").innerHTML = `There is no ${userInput.object}.`;
+            } 
+        } 
+        // WRONG INPUT
+        else {
+            document.getElementById("textOutput").innerHTML = "I don't know what you are trying to do.";
+        } 
+    }
     
 }
 
-// LOG USER INPUT AFTER ENTER KEY IS PRESSED
+
+// RUN FUNCTIONS ON ENTER
 
 userInput.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
-       
-        const userCommand = document.getElementById("userInput").value;
-        let numberOfWords = userCommand.split(" ").length
-        console.log(numberOfWords)
-        let userCommandVerb = userCommand.split(" ").shift();
-        let userCommandObjectTwo = userCommand.split(" ")[1];
-        let userCommandObject = userCommand.split(" ").pop();
-
-        if (numberOfWords > 2) {
-
-            // USE OBJECT ON OBJECT
-            if (objectUseWith.includes(userCommandVerb) && useWithObject.hasOwnProperty(userCommandObject) && useWithObject.hasOwnProperty(userCommandObjectTwo) && playerInventory.includes(userCommandObjectTwo)) {
-
-                document.getElementById("textOutput").innerHTML = 
-                useWithObject[userCommandObject];
-            }
-            
-            else {
-                document.getElementById("textOutput").innerHTML = "I don't know what you are trying to do.";
-            }
-        }
-        
-        else { 
-        // EXAMINE
-            if (objectExamine.includes(userCommandVerb) && examineObject.hasOwnProperty(userCommandObject)) {
-
-                document.getElementById("textOutput").innerHTML = 
-                examineObject[userCommandObject];
-            }
-
-            // INTERACT
-            else if (objectInteract.includes(userCommandVerb) && interactObject.hasOwnProperty(userCommandObject)) {
-
-                document.getElementById("textOutput").innerHTML = 
-                interactObject[userCommandObject];
-            }
-
-            // TAKE
-            else if (objectTake.includes(userCommandVerb) && takeObject.hasOwnProperty(userCommandObject)) {
-
-                document.getElementById("textOutput").innerHTML = 
-                takeObject[userCommandObject];
-
-                playerInventory += userCommandObject
-
-                // console.log(JSON.stringify(playerInventory));
-
-            }
-
-            // CAN'T TAKE
-
-            else if (objectTake.includes(userCommandVerb)) {
-                if (!(takeObject.hasOwnProperty(userCommandObject))) {
-                    if (examineObject.hasOwnProperty(userCommandObject)) {
-                        document.getElementById("textOutput").innerHTML = "You can't take the " + userCommandObject + ".";
-                    }
-                    else {
-                        document.getElementById("textOutput").innerHTML = "There is no " + userCommandObject + ".";
-                    }
-                    
-                }
-            }
-
-            // WRONG INPUT
-            else {
-                document.getElementById("textOutput").innerHTML = "I don't know what you are trying to do.";
-            } }
-        
-
-        
+        let userInputs = parseUserInput()
+        interpretUserInput(userInputs)
         userInput.value = ""
+    }
+})
 
-        }
 
         
-        
-        
-     
-});
 
 
-
+    
--- js/objectStringsRoomOne.js
+++ js/objectStringsRoomOne.js
@@ -1,30 +1,55 @@
 
-export const examineObject = {
+// export const examineObject = {
 
-    // initial presence strings
-    initInitRoom: "You wake up in an unfamiliar room. There is a table in the middle, and a wooden door on the far side.",
+//     // initial presence strings
+//     initInitRoom: "You wake up in an unfamiliar room. There is a table in the middle, and a wooden door on the far side.",
 
-    "table": "The table is made out of wood. There is a small key on it.",
-    "key": "A small, golden key.",
+//     "table": "The table is made out of wood. There is a small key on it.",
+//     "key": "A small, golden key.",
 
-    "door": "A wooden door. You don\'t know where it leads.",
-}
+//     "door": "A wooden door. You don\'t know where it leads.",
+// }
 
-export const interactObject = {
-    "door": "You try the door. It seems to be locked.",
+// export const interactObject = {
+//     "door": "You try the door. It seems to be locked.",
     
+// }
+
+// export const takeObject = {
+//     "key": "You take the key.",
+// }
+
+// export const useWithObject = {
+//     "key": "You unlock the door.",
+//     "door": "You unlock the door.",
+// }
+
+
+
+
+// export const goldenKey = {
+//     "examine": "A small, golden key.",
+//     "take": "You take the key."
+//     // "use with": "You unlock the door."
+// }
+
+
+
+export const smallTable = {
+    examineObject: "The table is made out of wood. There is a small key on it.",
 }
 
-export const takeObject = {
-    "key": "You take the key.",
+export const goldenKey = {
+    examineObject: "A small, golden key.",
+    takeObject: "You take the key.",
+    interactObject: "What do you want to use the key with?"
 }
 
-export const useWithObject = {
-    "key": "You unlock the door.",
-    "door": "You unlock the door.",
+export const firstDoor = {
+    examineObject: "A wooden door. You don\'t know where it leads.",
+    interactObject: "You try the door. It seems to be locked.",
 }
 
-
-
-// export default objectExamine
-// export altern objectInteract
+export const objectCombinations = new Map ([
+    [JSON.stringify([goldenKey, firstDoor]), "You unlock the door."],
+])
